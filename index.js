57425ffcfb0f53ef892adaa7b89578c3a425e5b6
Tim Jordan <raeyejordan@yahoo.com>
2018-01-18 19:58:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ var recipes={'cook book':"turkey", Grandmas:"cornbread",online:"cake"};
 function updateObjectWithKeyAndValue(obj,key,value){
   return Object.assign({},obj,{[key]:value});
 }
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+  obj[key]=value;
+  return obj;
+}
+function deleteFromObjectByKey(obj,key){
+  newObj=Object.assign({},obj);
+  delete newObj.key;
+  return newObj;
+}
+function destructivelyDeleteFromObjectByKey(obj,key){
+  delete obj.key;
+  return obj;
+}
